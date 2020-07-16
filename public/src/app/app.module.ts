23aa869388550ea2f054599f37d3534013cce75f
Simon Schaufelberger <dextter767@gmail.com>
2020-07-16 12:58:43 +0200

    Added SharedModule

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Custom Modules
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
