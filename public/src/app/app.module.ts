bcd1969840454bd6f64296401d072a8d11dcf4b6
Simon Schaufelberger <dextter767@gmail.com>
2020-07-16 13:20:16 +0200

    Added AngularMaterial NPM Package

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 // Custom Modules
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
