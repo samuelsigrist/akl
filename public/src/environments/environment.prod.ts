352d53b358eef2aec6511180476664b0cabae96d
Simon Schaufelberger <dextter767@gmail.com>
2020-07-22 09:52:01 +0200

    Added VideoUrl's to both environment files

--- public/src/environments/environment.prod.ts
+++ public/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  videoUrl: 'http://172.16.3.169/video/servus.mp4',
+  camOneUrl: 'http://172.19.24.27:8000/stream.mjpg',
+  camTwoUrl: 'http://172.19.28.28:8081/stream.mjpg'
 };
--- public/src/environments/environment.ts
+++ public/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  videoUrl: 'http://172.16.3.169/video/servus.mp4',
+  camOneUrl: 'http://172.19.24.27:8000/stream.mjpg',
+  camTwoUrl: 'http://172.19.28.28:8081/stream.mjpg'
 };
 
 /*
